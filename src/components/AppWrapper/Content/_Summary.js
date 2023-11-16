9655f4f7fda540d3f97280f4cba9cd362376a7cd
codingloop <ishunfs@gmail.com>
2023-11-16 22:41:15 +0530

    Add summary row

--- src/components/AppWrapper/Content/_Summary.js
+++ src/components/AppWrapper/Content/_Summary.js
@@ -1,21 +1,30 @@
+import { useContext } from "react";
 import Container from "./_Container";
 import style from "./style.module.css";
+import { UserContext } from "@/utils/providers/userInfo";
 
-const SummaryCard = () => (
+const SummaryCard = ({ item }) => (
   <div className={style.col_t2}>
     <div className={style.summary_frame}>
       <div className="min-w-[60px]">
-        <span>a345</span>
+        <span className={style.summary_frame_counter}>{item?.heading}</span>
       </div>
-      <h6>Helllot fd sd f</h6>
+      <h6>{item?.value}</h6>
     </div>
   </div>
 );
 
 function Summary() {
+  const userInfo = useContext(UserContext);
   return (
     <Container>
       <div className={`${style.row} pt-[30px] pb-0`}>
+        {userInfo.summary?.map((item, ind) => (
+          <SummaryCard key={`si_${ind}`} item={item} />
+        ))}
+        <SummaryCard />
+        <SummaryCard />
+        <SummaryCard />
         <SummaryCard />
       </div>
     </Container>
