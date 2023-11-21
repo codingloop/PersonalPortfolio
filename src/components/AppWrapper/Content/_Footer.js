67460641a76a1faca2abb7c6955aec964470c4e5
codingloop <ishunfs@gmail.com>
2023-11-21 22:30:03 +0530

    add responsivness to footer

--- src/components/AppWrapper/Content/_Footer.js
+++ src/components/AppWrapper/Content/_Footer.js
@@ -2,7 +2,7 @@ import style from "./style.module.css";
 
 function Footer() {
   return (
-    <div className="overflow-hidden p-0 w-full m-auto ml-[30px]">
+    <div className="overflow-hidden p-0 w-full m-auto ml-[30px] mw920:ml-0">
       <div className={style.footer}>
         <div className="mb-[10px] mx-auto">© 2023 Ishwar Bhat</div>
       </div>
