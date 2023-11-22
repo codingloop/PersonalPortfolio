604f7ab97cce0cdb2d8abfa1cd5c5acf0049c09a
codingloop <ishunfs@gmail.com>
2023-11-22 22:24:34 +0530

    add meta

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -3,6 +3,11 @@ import Head from "next/head";
 export default function AppHeader() {
   return (
     <Head>
+      <title>Ishwar Bhat - Portfolio</title>
+      <meta
+        name="description"
+        content="Ishwar Bhat, developer, AWS and python expert, web developer, react js and django core skills"
+      />
       <meta charSet="utf-8" />
       <meta
         name="viewport"
@@ -10,7 +15,6 @@ export default function AppHeader() {
       />
       <meta name="theme-color" content="#2B2B35" />
       <link rel="shortcut icon" href="img/thumbnail.ico" type="image/x-icon" />
-      <title>Portfolio - Ishwar</title>
     </Head>
   );
 }
