81cded24e370f1f521887a3e154d8b002018e98f
codingloop <ishunfs@gmail.com>
2023-11-29 23:50:50 +0530

    Adjust padding and margins

--- src/components/AppWrapper/Content/_Footer.js
+++ src/components/AppWrapper/Content/_Footer.js
@@ -2,9 +2,11 @@ import style from "./style.module.css";
 
 function Footer() {
   return (
-    <div className="overflow-hidden p-0 w-full m-auto ml-[30px] mw920:ml-0">
-      <div className={style.footer}>
-        <div className="mb-[10px] mx-auto">© 2023 Ishwar Bhat</div>
+    <div className={style.footer_container}>
+      <div className="overflow-hidden p-0 w-full m-auto mw920:ml-0">
+        <div className={style.footer}>
+          <div className="mb-[10px] mx-auto">© 2023 Ishwar Bhat</div>
+        </div>
       </div>
     </div>
   );
